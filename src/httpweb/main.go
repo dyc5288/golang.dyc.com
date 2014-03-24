b6274cbcfed45117a9879d868a1adfcc6be2ddcf
duanyunchao <dyc5288@163.com>
2014-03-24 16:35:18 +0000

    web

--- src/httpweb/main.go
+++ src/httpweb/main.go
@@ -1,28 +1,60 @@
 package main
 
+// http://www.ttbiji.com/
+
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"strings"
 )
 
-func sayhello(w http.ResponseWriter, r *http.Request) {
+func route() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/note", note)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("path", r.URL.Path)
+	if strings.HasPrefix(r.URL.Path, "/static") {
+		file := "static" + r.URL.Path[len("/static"):]
+		http.ServeFile(w, r, file)
+		return
+	}
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("path", r.URL.Path)
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("login.gtpl")
+		t.Execute(w, nil)
+	} else {
+
+	}
+}
+
+func note(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("path", r.URL.Path)
 	r.ParseForm()
 	fmt.Println(r.Form)
 	fmt.Println("path", r.URL.Path)
 	fmt.Println("scheme", r.URL.Scheme)
 	fmt.Println(r.Form["url_long"])
+
 	for k, v := range r.Form {
 		fmt.Println("key:", k)
 		fmt.Println("val:", strings.Join(v, ""))
 	}
-	fmt.Fprintf(w, "hello astaxie")
+
+	fmt.Fprintf(w, "hello note")
 }
 
 func main() {
-	http.HandleFunc("/", sayhello)
-	err := http.ListenAndServe(":9090", nil)
+	route()
+	err := http.ListenAndServe(":8888", nil)
+
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
